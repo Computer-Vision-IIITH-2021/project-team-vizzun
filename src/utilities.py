5375d66bed9e6fef693c68e54497adb998c90e01
Mohsin Mamoon <mohsin.mamoon@students.iiit.ac.in>
2021-04-24 23:06:19 +0530

    Added code for DatasetHandling

--- src/utilities.py
+++ src/utilities.py
@@ -3,8 +3,12 @@ import cv2
 import torch
 import argparse
 import numpy as np
+
 from PIL import Image
 from torchvision.utils import save_image
+from torchvision import transforms
+from torch.utils.data import DataLoader, SubsetRandomSampler
+from torchvision.datasets import ImageFolder
 
 def read_image(path, size=None, gray=False):
     img = cv2.imread(path)
@@ -28,6 +32,24 @@ def save_tensor_image(img, name, de_normalize=True):
     else:
         save_image(img, name)
 
+processTestImage = transforms.Compose([
+    transforms.Resize(512),
+    transforms.ToTensor(),
+    # transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+    transforms.Lambda(lambda x: x.unsqueeze(0))
+])
+
+processTrainImage = transforms.Compose([
+    transforms.Resize(256),
+    transforms.CenterCrop(224),
+    # transforms.ColorJitter(brightness=0, contrast=0, saturation=0, hue=0),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+])
+
+NameExtract = lambda path: ExtensionRemove(path[path.rfind('/')+1:])
+ExtensionRemove = lambda name: name[:name.rfind('.')]
+
 def Parser():
 
     parser = argparse.ArgumentParser()
@@ -44,3 +66,31 @@ def Parser():
     parser.add_argument("--alpha", help="Alpha value to control amount of styleTransfer", type=float, default=1)
     parser.add_argument("-w", "--weights", help="Weights for different styles (Style Interpolation)", default=None)
     return parser
+
+def getDataset(contentPath, stylePath, val=0.2, bs=64):
+
+    contentTrain = ImageFolder(contentPath, transform=processTrainImage)
+    styleTrain = ImageFolder(stylePath, transform=processTrainImage)
+
+    sz = min(len(contentTrain), len(styleTrain))
+
+    contentTrain, contentVal = getLoaders(contentTrain, size=sz, val=val, bs=bs)
+    styleTrain, styleVal = getLoaders(styleTrain, size=sz, val=val, bs=bs)
+    return contentTrain, contentVal, styleTrain, styleVal
+
+def getLoaders(dataset, size, val=0.2, bs=64):
+    
+    indices = list(range(size))
+    split = int(np.floor(val * size))
+    np.random.seed(1)
+    np.random.shuffle(indices)
+    train_indices, val_indices = indices[split:], indices[:split]
+
+    # Creating PT data samplers and loaders:
+    train_sampler = SubsetRandomSampler(train_indices)
+    valid_sampler = SubsetRandomSampler(val_indices)
+
+    train_loader = DataLoader(dataset, batch_size=bs, sampler=train_sampler)
+    val_loader = DataLoader(dataset, batch_size=bs, sampler=valid_sampler)
+
+    return train_loader, val_loader
