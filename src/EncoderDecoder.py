d8305a219d64af9f809b1a0878fbdd22702a4e3e
Kartik Agarwal <kartik.agarwal@students.iiit.ac.in>
2021-04-24 12:50:38 +0530

    Add files via upload

---
+++ src/EncoderDecoder.py
@@ -0,0 +1,18 @@
+import torch
+import torch.nn as nn
+
+class EncoderDecoder(nn.Module):
+    def __init__(self, encoder, decoder):
+        super().__init__()
+
+        self.encoder = encoder
+        self.decoder = decoder    
+        self.style_features = []
+        self.style_layers = [1, 6, 11, 20] # relu1_1, relu2_1, relu3_1, relu4_1
+        for i in self.style_layers:
+            self.encoder._modules[str(i)].register_forward_hook(self.style_feature_hook)
+
+    def style_feature_hook(self, module, input, output):
+        self.style_features.append(output)
+
+    
---
+++ src/main.py
@@ -0,0 +1,50 @@
+import sys
+import torch
+import torch.nn as nn
+
+from PIL import Image
+
+# from EncoderDecoder import EncoderDecoder
+from StyleTransfer import ContentStyleLoss, StyleTransfer
+from train import trainModel
+from utilities import save_tensor_image, processTestImage, NameExtract, Parser, getDataset
+
+
+TRAINED_WEIGHTS = "adain_trained"
+
+parser = Parser()
+args = parser.parse_args()
+
+if __name__ == '__main__':
+
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    
+    if (args.action == "run"):
+        contentImage = args.content_image
+        styleImage = args.style_image
+
+        contName = NameExtract(contentImage)
+        styleName = NameExtract(styleImage)
+
+        contentImage = processTestImage(Image.open(contentImage)).to(device)
+        styleImage = processTestImage(Image.open(styleImage)).to(device)
+
+        if args.model:
+            TRAINED_WEIGHTS = args.model
+        
+        model = StyleTransfer(device)
+        model.load_state_dict(torch.load(TRAINED_WEIGHTS))
+
+        styledImage = model(contentImage, styleImage)[0]
+        
+        save_tensor_image(styledImage, f"../outputs/{contName}_{styleName}.jpg")
+        save_tensor_image(styledImage, f"../outputs/{contName}_{styleName}_normalized.jpg", False)
+        print("Style Transfer completed! Please view", f"../outputs/{contName}_{styleName}.jpg")
+
+    
+
+    elif (args.action == "run_multiple_styles"):
+        pass
+    
+    elif (args.action == "run_alpha"):
+        pass
